ef28b3bde635912d6a847a1759c387362c2cf430
ls <1261972397@qq.com>
2015-09-18 20:43:51 +0800

    test2

---
+++ Test1/src/com/example/test1/MyActivity.java
@@ -0,0 +1,16 @@
+package com.example.test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     * test
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
